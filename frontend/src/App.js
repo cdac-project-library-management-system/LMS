33beef2542cfb749c508693917a7e232f1cbaec8
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-04 19:52:42 +0530

    initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/user/Login';
+import Register from './pages/user/Register';
+import ForgotPassword from './pages/user/ForgotPassword';
+import BorrowedBooks from './pages/user/BorrowingHistory';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import './styles/global.css'; // Import global styles
+import UserDashboard from './pages/user/UserDashboard';
+import BookInfo from './pages/user/BookInfo';
+import BrowseCategoriesPage from './pages/user/BrowseCategoriesPage';
+import BookReviewPage from './pages/user/BookReviewPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/browse-categories" element={<BrowseCategoriesPage />} />
+        <Route path="/borrowed-books" element={<BorrowedBooks />} />
+        <Route path="/review-book" element={<BookReviewPage />} />
+        <Route path="/book/:bookId" element={<BookInfo />} />
+        <Route path="*" element={<Login />} /> {/* Default to login */}
+      </Routes>
+    </Router>
   );
 }
 
