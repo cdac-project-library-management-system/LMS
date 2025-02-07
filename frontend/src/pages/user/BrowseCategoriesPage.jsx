b1ac9ee5267a5f64febd5a127064d3682efa8679
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-07 20:22:49 +0530

    feat: Add Categories component and page

---
+++ frontend/src/pages/user/BrowseCategoriesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import BrowseCategories from '../../components/user/BrowseCategories';
+import MainLayout from '../../components/user/layout/MainLayout';
+
+const BrowseCategoriesPage = () => {
+  return (
+    <MainLayout>
+      <BrowseCategories />
+    </MainLayout>
+  );
+};
+
+export default BrowseCategoriesPage;
