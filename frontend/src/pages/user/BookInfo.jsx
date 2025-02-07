4ad0a40e0e2cd6a38f05cb0918f2e7190cb37027
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-08 01:19:53 +0530

    feat: Add BookInfo component in the page

---
+++ frontend/src/pages/user/BookInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainLayout from "../../components/user/layout/MainLayout";
+import BookDetails from "../../components/user/BookDetails";
+
+const BookInfo = () => {
+    return (
+        <MainLayout>
+            <BookDetails />
+        </MainLayout>
+    );
+};
+
+export default BookInfo;
