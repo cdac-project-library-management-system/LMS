33061472e1b90cd880896284e9bd1d05a440d8ea
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-11 02:07:26 +0530

    feat: Added custom exception for UserValidation

---
+++ backend/Library_management_system/src/main/java/com/lms/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.lms.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message) {
+        super(message);
+    }
+}
