9197ef584ddb6c268257951081356604ebd1ac14
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-11 12:30:27 +0530

    delete: deleted ReviewService from the main

--- backend/Review Service/src/main/java/com/lms/review/service/ReviewService.java
+++
@@ -1,9 +0,0 @@
-package com.lms.review.service;
-
-import com.lms.review.dto.ReviewDTO;
-import java.util.List;
-
-public interface ReviewService {
-    ReviewDTO submitReview(ReviewDTO reviewDTO);
-    List<ReviewDTO> getReviewsByBookId(Long bookId);
-}
