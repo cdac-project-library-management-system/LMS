a070a7acb10bad9fee2ad7518469d6b562348f3e
Shrey Khodre <khodreshreyas123@gmail.com>
2025-02-11 14:24:11 +0530

    feat: Add CustomUserDetailsService class

---
+++ backend/Library_management_system/src/main/java/com/lms/security/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.lms.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.lms.entities.User;
+import com.lms.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new CustomUserDetails(user);
+    }
+}
